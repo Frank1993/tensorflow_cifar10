b76c9ea5cd638216fc7ee67da31ed318645b2c9d
Frank Hu <hupengcheng1993@126.com>
2016-10-21 13:22:04 +0800

    add cifar10_train.py

--- cifar10.py
+++ cifar10.py
@@ -24,6 +24,18 @@ tf.app.flags.DEFINE_integer("batch_size",200, "number of examples used in one ba
 
 DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
 
+# Global constants describing the CIFAR-10 data set.
+IMAGE_SIZE = cifar10_input.IMAGE_SIZE
+NUM_CLASSES = cifar10_input.NUM_CLASSES
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+# Constants describing the training process.
+MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
+NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
+
 def maybe_download_and_extract():
     dest_directory = FLAGS.data_dir
 
@@ -43,8 +55,12 @@ def maybe_download_and_extract():
 
         filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
 
+        statinfo = os.stat(filepath)
+        print("Successfully downloaded",filename,statinfo.st_size,"bytes")
 
-def input():
+        tarfile.open(filepath,'r:gz').extractall(dest_directory)
+        
+def inputs():
     if not FLAGS.data_dir:
         raise ValueError("Please supply a data dir")
 
@@ -91,6 +107,65 @@ def inference(images):
     # norm2
     norm2 = tf.nn.lrn(conv2,4,bias = 1.0, alpha = 0.001/9.0, beta = 0.75, name = "norm2")
 
+    # pool2
+    pool2 = tf.nn.max_pool(norm2,ksize = [1,3,3,1], strides = [1,2,2,1], padding = "SAME", name = "pool2")
+
+    #local3
+
+    with tf.variable_scope("local3") as scope:
+        # Move everything into depth so we can perform a single matrix multiply.
+        reshape = tf.reshape(pool2,[FLAGS.batch_size,-1])
+        dim = reshape.get_shape()[1].value
+        weights = tf.get_variable("weight",shape = [dim,384], initializer = tf.truncated_normal_initializer(1e-2))
+
+        biases = tf.get_variable("biases",[384],tf.constant_initializer(0.1))
+
+        local3 = tf.nn.relu(tf.matmul(reshape,weights) + biases,name = scope.name)
+
+    with tf.variable_scope("local4") as scope:
+        weights = tf.get_variable("weight", shape = [384,192],initializer = tf.truncated_normal_initializer(1e-2))
+
+        biases = tf.get_variable("biases",[192],tf.constant_initializer(0.1))
+
+        local4 = tf.nn.relu(tf.matmul(local3,weights) + biases, name = scope.name)
+
+    # softmax softmax(WX+b)
+    with tf.variable_scope("softmax_linear") as scope:
+        weights = tf.get_variable("weight", [192,NUM_CLASSES], initializer = tf.truncated_normal_initializer(1e-2))
+
+        biases = tf.get_variable("biases", [NUM_CLASSES],tf.constant_initializer(0.0))
+
+        softmax_linear = tf.add(tf.matmul(local4,weights), biases, name = scope.name)
+
+        return softmax_linear
+
+
+def loss(logits,labels):
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits,labels, name = "cross_entropy_per_example")
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name = "cross_entropy")
+    return cross_entropy_mean
+
+
+def train(loss,global_step):
+    num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
+
+    decay_steps = int(num_batches_per_epoch* NUM_EPOCHS_PER_DECAY)
+
+    lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,global_step,decay_steps,LEARNING_RATE_DECAY_FACTOR, staircase = true)
+
+    optimizer = tf.train.AdamOptimizer(lr)
+
+    minimize_op=optimizer.minimize(loss,global_step)
+
+    # Track the moving average of all trainable variable
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
+
+    variable_averages_op = variable_averages.apply(tf.traiable_variables())
+
+    with tf.control_dependencies([,variable_averages_op]):
+        train_op = tf.no_op(name = "train")
+
+    return train_op
 
 def _activation_summary(x):
     tensor_name = x.op.name
---
+++ cifar10_train.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+
+import os.path
+import time
+
+import numpy as np
+from six.moves import xrange
+import tensorflow as tf
+
+import cifar10
+
+
+FlGAS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string("train_dir","cifar10_data","""Directory where to write event logs and checkpoint.""")
+
+tf.app.flags.DEFINE_integer("max_steps", 1000,"""Number of batches to run""")
+
+def train():
+	with tf.Graph().as_default():
+		global_step = tf.Variable(0,trainable = False)
+
+		#get images and labels for cifar-10
+		images,labels = cifar10.inputs()
+
+		logits = cifar10.inference(images)
+
+		loss = cifar10.loss(logits,labels)
+
+		train_op = cifar10.train(loss,global_step)
+
+		with tf.Session() as sess:
+			init = tf.initialize_all_variables()
+
+			sess.run(init)
+
+			tf.train.start_queue_runners()
+
+			for step in xrange(FLAGS.max_steps):
+				_,loss_value = sess.run([train_op,loss])
+
+				if step % 10 == 0:
+					print("Step %s: loss = %s"%(step,loss_value))
+
+def main(argv = None):
+	cifar10.maybe_download_and_extract()
+
+	if tf.gfile.Exists(FlGAS.train_dir):
+		tf.gfile.DeleteRecursively(FLAGS.train_dir)
+
+	tf.gfile.MakeDirs(FLAGS.train_dir)
+
+	train()
+
+if __name__ == "__main__":
+	tf.app.run()
